cb683898e0d52beb8b687ab210dc2f68437bdf5e
Matthew Crouch <mcrouch@(null).(none)>
2012-04-19 19:30:40 +0100

    Initial Commit

---
+++ PandaTube/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PandaTube
+//
+//  Created by Matthew Crouch on 19/04/2012.
+//  Copyright (c) 2012 Crazy Panda Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PandaTube/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PandaTube
+//
+//  Created by Matthew Crouch on 19/04/2012.
+//  Copyright (c) 2012 Crazy Panda Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
